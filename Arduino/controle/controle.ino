a419334dd6ad66b6c6229512a8e1682b9465518f
Athila Raffael Silva Batista <raffael.silva@escolar.ifrn.edu.br>
2025-07-07 08:55:21 -0300

    Update controle.ino

--- Arduino/controle/controle.ino
+++ Arduino/controle/controle.ino
@@ -5,58 +5,56 @@ uint8_t Motor[4] = {4, 5, 6, 7};
 
 int maxValue[2];
 int minValue[2];
-int maxValueConta[2];
-int minValueConta[2];
-int NumMin[2], NumMax[2]; 
+
 int values[2];
 
 int P, I, D, erroAnt, PID;
 
-float Kp = 10, Kd = 1.5, Ki = 0.01;
+float Kp = 10.0, Kd = 1.5, Ki = 0.01;
 
 uint8_t baseSpeed = 90;
 uint8_t Vd, Ve;
 
-
 void setup() {
-
   Serial.begin(9600);
 
-  Serial.println("Indo");
+  Serial.println("Iniciando");
 
-  for(uint8_t x = 0; x < 4; x++){
+  for (uint8_t x = 0; x < 4; x++) {
     pinMode(Motor[x], OUTPUT);
   }
 
-  Serial.println("motores ok");
+  Serial.println("Motores ok");
 
   pinMode(PWMd, OUTPUT);
   pinMode(PWMe, OUTPUT);
   pinMode(2, INPUT_PULLUP);
 
   Serial.println("Pinos OK");
-  
+
   analogWrite(PWMd, baseSpeed);
   analogWrite(PWMe, baseSpeed);
 
   calibrar();
 
-  while(true){
-    if(digitalRead(2) == LOW){
+  Serial.println("Aguardando botão no pino 2...");
+
+  while (true) {
+    if (digitalRead(2) == LOW) {
+      Serial.println("Iniciando controle");
       break;
     }
   }
-
 }
 
 void loop() {
-
+  // Configura os motores para frente (ajusta conforme teu motor)
   digitalWrite(Motor[0], LOW);
   digitalWrite(Motor[1], HIGH);
   digitalWrite(Motor[2], HIGH);
   digitalWrite(Motor[3], LOW);
 
-  ler();
+  lerSensores();
 
   int erro = values[0] - values[1];
 
@@ -68,87 +66,79 @@ void loop() {
 
   PID = (Kp * P) + (Ki * I) + (Kd * D);
 
+  // Limita PID para não dar pau no motor
+  if (PID > 255) PID = 255;
+  else if (PID < -255) PID = -255;
 
-  Ve = baseSpeed - PID;
-  Vd = baseSpeed + PID;
+  Ve = constrain(baseSpeed - PID, 0, 255);
+  Vd = constrain(baseSpeed + PID, 0, 255);
 
   analogWrite(PWMd, Vd);
   analogWrite(PWMe, Ve);
 
-  //Serial.print(erro);
-  /*Serial.print(" ");
+  // Debug no serial
+  Serial.print("Erro: ");
+  Serial.print(erro);
+  Serial.print(" | Vd: ");
   Serial.print(Vd);
-  Serial.print(" ");
-  Serial.print(Ve);*/
-  //Serial.print(" ");
-  Serial.print(analogRead(A0));
-  Serial.print(" ");
-  Serial.print(minValue[0]);
-  Serial.print(" ");
-  Serial.print(analogRead(A1));
-  Serial.print(" ");
-  Serial.println(minValue[1]);
-  
+  Serial.print(" | Ve: ");
+  Serial.print(Ve);
+  Serial.print(" | S0: ");
+  Serial.print(values[0]);
+  Serial.print(" | S1: ");
+  Serial.println(values[1]);
 
+  delay(10); // pequeno delay pra não lotar o serial
 }
 
-void calibrar(){
+void calibrar() {
+  Serial.println("Iniciando calibração...");
 
-  Serial.println("Iniciando calibração");
-
-  for(uint8_t x = 0; x < 2; x++){
-    minValueConta[x] = analogRead(x);
-    maxValueConta[x] = analogRead(x);
+  // Inicializa os min e max
+  for (uint8_t i = 0; i < 2; i++) {
+    minValue[i] = 1023;
+    maxValue[i] = 0;
   }
-  
-  Serial.println("p2");
 
+  // Liga motores em sentido contrário pra passar na linha (ajusta conforme teu motor)
   digitalWrite(Motor[0], LOW);
   digitalWrite(Motor[1], HIGH);
   digitalWrite(Motor[2], LOW);
   digitalWrite(Motor[3], HIGH);
 
-  for(int x = 0; x < 10000; x++){
-    for(uint8_t i = 0; i < 2; i++){
-      
+  // Lê várias vezes para pegar min e max reais
+  for (int x = 0; x < 10000; x++) {
+    for (uint8_t i = 0; i < 2; i++) {
       int leitura = analogRead(i);
-      
-      if(leitura < minValueConta[i]){
-        minValueConta[i] = leitura;
-        minValue[i] += leitura;
-        NumMin[i]++;
-      }
-
-      if(leitura > maxValueConta[i]){
-        maxValueConta[i] = leitura;
-        maxValue[i] += leitura;
-        NumMax[i]++;
-      }
+      if (leitura < minValue[i]) minValue[i] = leitura;
+      if (leitura > maxValue[i]) maxValue[i] = leitura;
     }
     delay(1);
   }
-  
-  Serial.println("P3");
 
-  for(uint8_t x = 0; x < 2; x++){
-    minValue[x] = minValue[x] / NumMin[x];
-    maxValue[x] = maxValue[x] / NumMax[x];
+  // Desliga motores
+  for (uint8_t x = 0; x < 4; x++) {
+    digitalWrite(Motor[x], LOW);
   }
 
-  digitalWrite(Motor[0], LOW);
-  digitalWrite(Motor[1], LOW);
-  digitalWrite(Motor[2], LOW);
-  digitalWrite(Motor[3], LOW);
-
-  Serial.println("Calibrado");
+  Serial.println("Calibração feita:");
+  for (uint8_t i = 0; i < 2; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(" Min: ");
+    Serial.print(minValue[i]);
+    Serial.print(" Max: ");
+    Serial.println(maxValue[i]);
+  }
 }
 
-
-
-void ler(){
-  
-  for(int i = 0; i < 2; i++){
+void lerSensores() {
+  for (int i = 0; i < 2; i++) {
+    // Mapeia leitura entre 0 e 100 baseado na calibração
     values[i] = map(analogRead(i), minValue[i], maxValue[i], 0, 100);
-    //values[i] = map(analogRead(i), 0, 1023, 0, 100);
-  } 
+
+    // Garantir que não passe dos limites
+    if (values[i] < 0) values[i] = 0;
+    if (values[i] > 100) values[i] = 100;
+  }
 }
