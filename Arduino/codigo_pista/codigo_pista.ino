3ee8f0e467b54624e5972b8154bf0be8f60f16ca
Athila <raffael.silva@escolar.ifrn.edu.br>
2025-11-25 21:11:28 -0300

    Update codigo_pista.ino

--- Arduino/codigo_pista/codigo_pista.ino
+++ Arduino/codigo_pista/codigo_pista.ino
@@ -7,8 +7,11 @@ uint16_t init_time2 = 0;
 uint16_t total_time1 = 0;
 uint16_t total_time2 = 0;
 
+bool ant2 = true, ant3 = true, ant4 = true, ant5 = true;
+
 void setup() {
   Serial.begin(9600);
+  Serial.print("Começando");
 
   for(uint8_t x = 0; x < 4; x++){
     pinMode(pins_sensor[x], INPUT);
@@ -18,7 +21,7 @@ void setup() {
   for(uint8_t y = 0; y < 3; y++){
     digitalWrite(pins_output[y], HIGH);
     digitalWrite(9, HIGH);
-    delay(700);
+    delay(1000);
     digitalWrite(9, LOW);
     delay(300);
   }
@@ -26,22 +29,29 @@ void setup() {
 }
 
 void loop() {
-  if(pins_sensor[0] == LOW){
+
+
+  if(digitalRead(pins_sensor[0]) != ant2){
     init_time1 = millis();
   }
-  if(pins_sensor[2] == LOW){ 
+  if(digitalRead(pins_sensor[2]) != ant3){ 
     init_time2 = millis();
   }
 
-  if(init_time1 > 0 && pins_sensor[1] == LOW){
+  if(init_time1 > 0 && digitalRead(pins_sensor[1]) != ant4){
     total_time1 = millis() - init_time1;
     Serial.print("Tempo do carro 1: ");
     Serial.println(total_time1);
   }
 
-  if(init_time2 > 0 && pins_sensor[3] == LOW){
+  if(init_time2 > 0 && digitalRead(pins_sensor[3]) != ant5){
     total_time2 = millis() - init_time2;
     Serial.print("Tempo do carro 2: ");
     Serial.println(total_time2); 
   }
+
+  ant2 = digitalRead(pins_sensor[0]);
+  ant3 = digitalRead(pins_sensor[1]);
+  ant4 = digitalRead(pins_sensor[2]);
+  ant5 = digitalRead(pins_sensor[3]);
 }
